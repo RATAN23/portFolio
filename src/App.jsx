e2be874d4d68e3eb1fc21247b420db8c653ae674
RATAN23 <rnghasti42@gmail.com>
2024-07-14 01:09:49 +0530

    Fixed the duplicacy of About Me section

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <div className="h-screen ">
         <Navbar />
         {/* Header Section */}
-        <AboutMe />
+        <Header />
 
         {/* About me Section */}
         <section className="snap-start">
